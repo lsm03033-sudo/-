69654b97318f433c23e52368451b3246c08b4cde
lsm03033-sudo <lsm03033@gmail.com1>
2026-07-29 21:01:18 +0900

    하트 애니메이션 추가

--- static/script.js
+++ static/script.js
@@ -33,3 +33,26 @@ btn.addEventListener("click", () => {
     },1000);
 
 });
+
+function createHeart() {
+
+    const heart = document.createElement("div");
+
+    heart.className = "heart";
+
+    heart.innerHTML = "❤️";
+
+    heart.style.left = Math.random() * 100 + "vw";
+
+    heart.style.animationDuration =
+        4 + Math.random() * 4 + "s";
+
+    document.getElementById("hearts").appendChild(heart);
+
+    setTimeout(() => {
+        heart.remove();
+    }, 8000);
+
+}
+
+setInterval(createHeart, 350);
